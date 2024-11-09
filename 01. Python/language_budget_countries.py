65af21308226597479b3e09a734f42f04dc52b15
Hugo Gómez Sabucedo <hugogomezsabucedo@gmail.com>
2024-11-09 18:55:07 +0100

    completado apartado F

---
+++ 01. Python/language_budget_countries.py
@@ -0,0 +1,39 @@
+import sys
+from mrjob.job import MRJob
+from collections import defaultdict
+
+class MRLanguageCountriesBudgets(MRJob):
+    def conv_int(cadena):
+        try:
+            return int(cadena)
+        except:
+            pass
+
+    def mapper(self, _, linea):
+        titulo, anho, idioma, pais, presupuesto = linea.split('|')
+        try:
+            anho = int(anho)
+            presupuesto = int(presupuesto)
+        except:
+            pass
+        
+        if idioma and pais and presupuesto and anho \
+            and presupuesto != -1 and anho != -1:
+            yield idioma, (pais, int(presupuesto))
+    
+    def reducer(self, idioma, values):
+        pais_presupuesto = defaultdict(int)
+        for pais, presupuesto in values:
+            pais_presupuesto[pais] += presupuesto
+        yield idioma, (list(pais_presupuesto.keys()),
+                       sum(pais_presupuesto.values()))
+
+# Para permitir la ejecucion por linea de comandos
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("El programa debe tomar 3 argumentos:")
+        print("language_budget_countries.py -q archivo_entrada")
+        sys.exit()
+    
+    archivo = sys.argv[2]
+    MRLanguageCountriesBudgets.run()
