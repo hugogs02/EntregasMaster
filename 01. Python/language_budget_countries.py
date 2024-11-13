54bb00ca025a876b4810d0b86cf9b843638b3540
Hugo Gómez Sabucedo <hugogomezsabucedo@gmail.com>
2024-11-14 00:31:43 +0100

    COMPLETADO

--- 01. Python/language_budget_countries.py
+++ 01. Python/language_budget_countries.py
@@ -4,30 +4,36 @@ from collections import defaultdict
 
 class MRLanguageCountriesBudgets(MRJob):
     def mapper(self, _, linea):
+        # Para cada línea, obtenemos sus componentes, que están separados por |
         titulo, anho, idioma, pais, presupuesto = linea.split('|')
+        
+        # Intentamos convertir a int el año y el presupuesto
         try:
             anho = int(anho)
             presupuesto = int(presupuesto)
         except:
             pass
         
+        # Si se conocen los datos de la película (es decir, existen el idioma y
+        # país y el presupuesto y año existen o son distintos de -1)
         if idioma and pais and presupuesto and anho \
             and presupuesto != -1 and anho != -1:
+            # Generamos los pares de clave-valor, con idioma como clave, y
+            # el país y presupuesto como valor
             yield idioma, (pais, int(presupuesto))
     
     def reducer(self, idioma, values):
+        # Generamos un defaultdict para el presupuesto 
         pais_presupuesto = defaultdict(int)
+        # Iteramos sobre los valores recibidos del mapper
         for pais, presupuesto in values:
+            # Sumamos el presupuesto de cada país
             pais_presupuesto[pais] += presupuesto
+        # Producimos los pares clave-valor, donde la clave es el idioma, y
+        # los valores una lista con cada país y la suma de los presupuestos
         yield idioma, (list(pais_presupuesto.keys()),
                        sum(pais_presupuesto.values()))
 
 # Para permitir la ejecucion por linea de comandos
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("El programa debe tomar 3 argumentos:")
-        print("language_budget_countries.py -q archivo_entrada")
-        sys.exit()
-    
-    archivo = sys.argv[2]
     MRLanguageCountriesBudgets.run()
--- 01. Python/mvp_seasons.py
+++ 01. Python/mvp_seasons.py
@@ -2,7 +2,21 @@ import sys
 from mrjob.job import MRJob
 from collections import defaultdict
 
-def procesaArchivo(line):
+def procesaArchivo(line: str) -> tuple[str, str, float, float, float, float]:
+    """
+    Recibe una línea de un archivo de texto, con los campos separados por \
+    comas, y devuelve los siguientes campos de interés: temporada, nombre del \
+    jugador, puntos anotado, rebotes cogidos, asistencias dadas y net rating.
+
+    Parameters
+    ----------
+        line : La línea del archivo.
+
+    Returns
+    ----------
+        tuple[str, str, float, float, float, float] : Una tupla con los campos\
+        de interés.
+    """
     linea = line.split(',')
     temporada = linea[1]
     jugador = linea[0]
@@ -13,27 +27,41 @@ def procesaArchivo(line):
 
     return temporada, jugador, pts, reb, ast, net
 
-def calculaValor(p, r, a, n):
+def calculaValor(p: float, r: float, a: float, n: float) -> float:
+    """
+    Función que recibe los datos necesarios para calcular la valoración de un \
+    jugador (puntos, rebotes, asistencias y rating) y la calcula.
+
+    Parameters
+    ----------
+        p : Los puntos anotados por el jugador.
+        r : Los rebotes cogidos por el jugador.
+        a : Las asistencias dadas por el jugador.
+        n : El net rating del jugador.
+
+    Returns
+    ----------
+        float : La valoración de dicho jugador.
+    """
     valor = p*0.5+r*0.3+a*0.2
     return round(valor*(1+n*0.1),3)
 
 class MRLanguageCountriesBudgets(MRJob):
     def mapper(self, _, linea):
+        # Procesamos el archivo
         temporada, jugador, pts, reb, ast, net = procesaArchivo(linea)
+        # Calculamos la valoración de cada jugador 
         eff = calculaValor(pts, reb, ast, net)
         
+        # Generamos los pares clave (temporada) y valor (valoración
+        # y nombre del jugador)
         yield temporada, (eff, jugador)
     
     def reducer(self, temporada, values):
+        # Hallamos el máximo de valoración por temporada
         mvp = max(values)
         yield temporada, mvp
 
 # Para permitir la ejecucion por linea de comandos
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("El programa debe tomar 3 argumentos:")
-        print("language_budget_countries.py -q archivo_entrada")
-        sys.exit()
-    
-    archivo = sys.argv[2]
     MRLanguageCountriesBudgets.run()
