f0110a26d8fea9c4e66cfc3b4b2c653f71767145
Hugo Gómez Sabucedo <hugogomezsabucedo@gmail.com>
2025-03-18 20:41:04 +0100

    suavizado

--- 07.3 Minería de datos/codigoMineria3.py
+++ 07.3 Minería de datos/codigoMineria3.py
@@ -1,356 +1,165 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import datetime as dt
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
+from tabulate import tabulate
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
 
+# Cargamos y formateamos la fecha
+viajeros = pd.read_excel('viajerosMD.xlsx')
+viajeros['Fecha'] = viajeros['Fecha'].str.strip()
+viajeros['Fecha'] = pd.to_datetime(viajeros['Fecha'], format='%YM%m')
+viajeros['Viajeros'] = viajeros['Viajeros'].apply(pd.to_numeric, errors='coerce')
+viajeros.set_index('Fecha', inplace=True)
+viajeros.dropna(inplace=True)
+viajeros = viajeros.iloc[::-1]  # Invertimos la serie, ya que los datos van de 2024 para atras
 
-# Importamos el archivo, extraemos las variables
-penguins = sns.load_dataset("penguins")
-penguins.head()
-variables = penguins.columns
-variables_numericas = [x for x in variables if x not in ['species', 'island', 'sex']]
-penguins_nums = penguins[variables_numericas]
-
-# Obtenemos los descriptivos
-descriptivos = penguins_nums.describe().T
-for num in variables_numericas:
-    descriptivos.loc[num, "Rango"] = np.ptp(penguins_nums[num].dropna().values)
-    descriptivos.loc[num, "Asimetria"] = penguins_nums[num].skew()
-    descriptivos.loc[num, "Varianza"] = penguins_nums[num].var()
-    descriptivos.loc[num, "Coef. Variación"] = (penguins_nums[num].std() / penguins_nums[num].mean() )
-    descriptivos.loc[num, "Missing"] = (penguins_nums[num].isna().sum())
-
-print(descriptivos.to_string())
-
-# Eliminamos las filas con variablers NAN (solo hay 11, no perdemos cantidad significativa de datos)
-penguins.dropna(inplace=True)
-penguins.isna().sum()
-
-# Mapeamos el género
-penguins['sex'] = penguins['sex'].map({'Male': 1, 'Female': 0})
-
-# Quitamos la especie y la isla
-penguins_original = penguins.copy()
-penguins.drop(['species', 'island'], axis=1, inplace=True)
-variables = list(penguins.columns)
-
-# Dibujamos la matriz de correlación
-corr = penguins.corr()
-plt.figure(figsize=(10, 8))
-sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.2f')
+# Hacemos un gráfico con la serie
+viajeros.plot(y='Viajeros', figsize=(10,5))
+plt.title("Viajeros transportados en Media Distancia")
+plt.xlabel("Mes")
+plt.ylabel("Viajeros (miles)")
 plt.show()
 
-###################################
-### ANALISIS PCA
-# Estandarizacion de los datos
-penguins_std = pd.DataFrame(StandardScaler().fit_transform(penguins),
-    columns=['{}_z'.format(variable) for variable in variables], index=penguins.index )
-penguins_std.head()
+# Representamos los valores por año
+viajeros['Año'] = viajeros.index.year.astype(str)
+plt.figure(figsize=(12, 8))
+sns.lineplot(x=viajeros.index.month, y=viajeros.Viajeros, hue = viajeros['Año'], palette='Spectral')
+plt.xlabel('Mes')
+plt.ylabel('Estacionalidad')
+plt.title('Gráfico estacional por año: Viajeros de Media Distancia')
+plt.legend(title='Año', loc='upper left', bbox_to_anchor=(1, 1))
+plt.show()
+viajeros.drop('Año', axis=1, inplace=True) # Eliminamos la columna que habíamos añadido
 
-#Creamos el PCA y lo aplicamos
-pca1 = PCA(n_components=4)
-fit1 = pca1.fit(penguins_std)
-autovalores = fit1.explained_variance_ # Obtenemos autovalores
-autovalores
+# Hacemos la descomposición estacional aditiva
+add_decomp = seasonal_decompose(viajeros, model='additive', period=12)
+plt.rc("figure", figsize=(16,12))
+fig = add_decomp.plot()
 
-var_explicada = fit1.explained_variance_ratio_ #Obtenemos varianza explicada
-var_explicada
-
-var_acumulada = np.cumsum(var_explicada) #Varianza explicada según agregamos componentes
-var_acumulada
-
-# Creamos dataframe y representamos
-data = {'Autovalores': autovalores, 'Var.Explicada': var_explicada, 'Var. Acumulada': var_acumulada}
-tabla = pd.DataFrame(data, index=['Componente {}'.format(i) for i in range(1, fit1.n_components_+1)])
-print(tabla)
-
-plot_varianza_explicada(var_explicada, fit1.n_components_)
-
-#Repetimos el PCA con los dos componentes elegidos
-pca = PCA(n_components=2)
-fit = pca.fit(penguins_std)
-print(pca.components_)
-
-autovectores = pd.DataFrame(pca.components_.T,
-                            columns = ['Autovector {}'.format(i) for i in range(1, fit.n_components_+1)],
-                            index = ['{}_z'.format(variable) for variable in variables])
-print(autovectores)
-
-# Calculamos las dos primeras componentes principales
-resultados_pca = pd.DataFrame(fit.transform(penguins_std),
-                              columns=['Componente {}'.format(i) for i in range(1, fit.n_components_+1)],
-                              index=penguins_std.index)
-
-# Añadimos las componentes principales a la base de datos estandarizada.
-penguins_z_cp = pd.concat([penguins_std, resultados_pca], axis=1)
-print(penguins_z_cp)
-
-# Calculamos correlacion de las variables originales con las componentes elegidas
-variables_cp = penguins_z_cp.columns
-
-# Calculamos las correlaciones y seleccionamos las que nos interesan (variables contra componentes).
-correlacion = pd.DataFrame(np.corrcoef(penguins_std.T, resultados_pca.T),
-                           index = variables_cp, columns = variables_cp)
-
-n_variables = fit.n_features_in_
-correlaciones_penguins_con_cp = correlacion.iloc[:fit.n_features_in_, fit.n_features_in_:]
-print(correlaciones_penguins_con_cp)
-
-cos2 = correlaciones_penguins_con_cp**2
-print(cos2)
-
-# Dibujamos los diferentes graficos
-plot_cos2_heatmap(cos2)
-plot_corr_cos(fit.n_components, correlaciones_penguins_con_cp)
-plot_cos2_bars(cos2)
-plot_pca_scatter(pca, penguins_std, fit.n_components)
-
-# Dibujamos un scatter plot pero marcando las especies 
-componentes_principales = pca.transform(penguins_std)
-n_components = componentes_principales.shape[1]
-
-#Este bloque, donde se obtiene el gráfico por especie y sexo, y el siguiente, donde se 
-#obtiene el índice, se encuentran comentados para poder correr el script sin alterar los datos
-"""penguins_std['species'] = penguins_original['species']
-penguins_std['sex'] = penguins_original['sex'].map({0: 'male', 1: 'female'})
-
-for i in range(n_components):
-    for j in range(i + 1, n_components):  # Evitar pares duplicados
-        # Crear el gráfico de dispersión
-        plt.figure(figsize=(10, 6))  # Ajusta el tamaño de la figura si es necesario
-        
-        # Colorear los puntos según la especie y cambiar la forma según el sexo
-        sns.scatterplot(
-            x=componentes_principales[:, i],  # Eje X: Componente i
-            y=componentes_principales[:, j],  # Eje Y: Componente j
-            hue=penguins_std['species'],  # Colorear por especie
-            style=penguins_std['sex'],    # Cambiar forma según el sexo
-            palette='viridis',  # Paleta de colores para las especies
-            s=100,              # Tamaño de los puntos
-            markers={'male': 'o', 'female': 's'}  # Marcadores para machos y hembras
-        )
-        
-        # Dibujar líneas discontinuas que representen los ejes
-        plt.axhline(0, color='black', linestyle='--', linewidth=0.8)
-        plt.axvline(0, color='black', linestyle='--', linewidth=0.8)
-        
-        # Etiquetar los ejes
-        plt.xlabel(f'Componente Principal {i + 1}')
-        plt.ylabel(f'Componente Principal {j + 1}')
-        
-        # Establecer el título del gráfico
-        plt.title(f'Gráfico de Dispersión de Observaciones en PCA')
-        
-        # Mostrar la leyenda
-        plt.legend(title='Especie/Sexo', bbox_to_anchor=(1.05, 1), loc='upper left')
-        
-        plt.show()"""
-        
-# Calculamos los valores de CP1 y CP2 para Adelie y el valor del indice
-"""w1 = cos2['Componente 1'].sum()  # Peso para CP1
-w2 = cos2['Componente 2'].sum()  # Peso para CP2
-resultados_pca = pd.DataFrame(componentes_principales, columns=['Componente 1', 'Componente 2'])
-resultados_pca['Índice'] = (w1 * resultados_pca['Componente 1']) + (w2 * resultados_pca['Componente 2'])
-
-adelie_data = penguins_std[penguins_std['species'] == 'Adelie']
-adelie_cp = pca.transform(adelie_data.drop(columns=['species', 'sex']))
-adelie_cp_df = pd.DataFrame(adelie_cp, columns=['Componente 1', 'Componente 2'])
-adelie_cp_df['Índice'] = (w1 * adelie_cp_df['Componente 1']) + (w2 * adelie_cp_df['Componente 2'])
-print(f"Índice Adelie: {adelie_cp_df['Índice'].mean()}")
-
-adelie_data = penguins_std[penguins_std['species'] == 'Gentoo']
-adelie_cp = pca.transform(adelie_data.drop(columns=['species', 'sex']))
-adelie_cp_df = pd.DataFrame(adelie_cp, columns=['Componente 1', 'Componente 2'])
-adelie_cp_df['Índice'] = (w1 * adelie_cp_df['Componente 1']) + (w2 * adelie_cp_df['Componente 2'])
-print(f"Índice Adelie: {adelie_cp_df['Índice'].mean()}")"""
-
-##############################################################################
-############### CLUSTERING #############
-############### JERARQUICO #############
-########################################
-
-# Creamos un mapa de calor
-sns.clustermap(penguins, cmap='coolwarm', annot=True)
+# Representamos la tendencia y la serie ajustada estacionalmente
+viajeros_ajustada = viajeros['Viajeros'] - add_decomp.seasonal
+plt.figure(figsize=(12, 8))
+plt.plot(viajeros, label='Datos', color='gray')
+plt.plot(add_decomp.trend, label='Tendencia', color='blue') # Tendencia
+plt.plot(viajeros_ajustada, label='Estacionalmente ajustada', color='red') # Serie ajustada
+plt.xlabel('Fecha')
+plt.ylabel('Viajeros')
+plt.title('Viajeros en Media Distancia')
+plt.legend(loc='best')
 plt.show()
 
-# Calculamos la matriz de distancias y la representamos visualmente
-distance_matrix = distance.cdist(penguins_std, penguins_std, 'euclidean')
-print("Distance Matrix:")
-distance_small = distance_matrix[:5, :5]
-#Index are added to the distance matrix
-distance_small = pd.DataFrame(distance_small, index=penguins_std.index[:5], columns=penguins_std.index[:5])
+# Hacemos la descomposición estacional multiplicativa
+mult_decomp = seasonal_decompose(viajeros, model='multiplicative', period=12)
+plt.rc("figure", figsize=(16,12))
+fig = mult_decomp.plot()
 
-distance_small_rounded = distance_small.round(2)
-print(distance_small_rounded)
-penguins_std[:2]
+# Separamos los datos en train y test, guardando el último año como test
+train = viajeros[:-12]
+test = viajeros[-12:]
 
-plt.figure(figsize=(8, 6))
-penguins_distance = pd.DataFrame(distance_matrix, index = penguins_std.index, columns = penguins_std.index)
-sns.heatmap(penguins_distance, annot=False, cmap="YlGnBu", fmt=".1f")
-plt.show()
-
-# Obtenemos la matriz de enlace ??¿?¿?¿
-linkage = sns.clustermap(penguins_distance, cmap="YlGnBu", fmt=".1f", annot=False, method='average').dendrogram_row.linkage
-
-order = pd.DataFrame(linkage, columns=['cluster_1', 'cluster_2', 'distance', 'new_count']).index
-reordered_data = penguins_std.reindex(index=order, columns=order)
-sns.heatmap(reordered_data, cmap="YlGnBu", fmt=".1f", cbar=False)
-plt.show()    
-
-# Obtenemos el dendograma
-linkage_matrix = sch.linkage(penguins_std, method='ward')
-dendrogram = sch.dendrogram(linkage_matrix, labels=penguins.index, leaf_font_size=9, leaf_rotation=90, color_threshold=15)
-plt.show()
-
-## Se eligen los clusteres: 4
-
-# Asignamos cada observación a uno de los clusteres
-num_clusters = 4
-cluster_assignments = sch.fcluster(linkage_matrix, num_clusters, criterion='maxclust')
-print("Cluster Assignments:", cluster_assignments)
-plt.show()
-
-penguins['Cluster4'] = cluster_assignments
-print(penguins["Cluster4"])
-
-
-# Validamos el agrupamiento
-pca = PCA(n_components=2)
-principal_components = pca.fit_transform(penguins)
-penguins_pca = pd.DataFrame(data=principal_components, columns=['PC1', 'PC2'])
-plt.figure(figsize=(10, 6))
-
-# Loop through unique cluster assignments and plot data points with the same color
-for cluster in np.unique(cluster_assignments):
-    plt.scatter(penguins_pca.loc[cluster_assignments == cluster, 'PC1'],
-                penguins_pca.loc[cluster_assignments == cluster, 'PC2'],
-                label=f'Cluster {cluster}')
-# Add labels to data points
-for i, row in penguins_pca.iterrows():
-    plt.text(row['PC1'], row['PC2'], str(penguins.index[i]), fontsize=8)
-
-plt.title("Gráfico de PCA 2D con asignaciones de cluster")
-plt.xlabel("Componente Principal 1")
-plt.ylabel("Componente Principal 2")
+plt.figure(figsize=(12, 8))
+plt.plot(train, label='Train', color='gray')
+plt.plot(test, label='Test', color='yellow')
 plt.legend()
-plt.grid()
+plt.xlabel('Fecha')
+plt.ylabel('Viajeros')
 plt.show()
 
-############### CLUSTERING ####################################################
-############## NO JERARQUICO #############
-##########################################
-# Elegimos 4 clusters
-num_c=4
-kmeans = KMeans(n_clusters=num_c, random_state=0)
-kmeans.fit(penguins_std)
-kmeans_cluster_labels = kmeans.labels_
-print(kmeans_cluster_labels)
+# Aplicamos el suavizado exponencial simple
+model = SimpleExpSmoothing(train, initialization_method="estimated").fit()
+fcast = model.forecast(12)
 
-# Creamos el gráfico
-plt.figure(figsize=(10, 6))
-
-for cluster in np.unique(kmeans_cluster_labels):
-    plt.scatter(penguins_pca.loc[kmeans_cluster_labels == cluster, 'PC1'],
-                penguins_pca.loc[kmeans_cluster_labels == cluster, 'PC2'],
-                label=f'Cluster {cluster}')
-
-for i, row in penguins_pca.iterrows():
-    plt.text(row['PC1'], row['PC2'], str(penguins.index[i]), fontsize=8)
-
-plt.title("Gráfico de PCA 2D con asignaciones K-means")
-plt.xlabel("Componente Principal 1")
-plt.ylabel("Componente Principal 2")
+plt.figure(figsize=(12, 8))
+# Serie original
+plt.plot(train, label='Datos train', color='gray')
+plt.plot(test, label='Datos test', color='yellow')
+plt.plot(model.fittedvalues, label='Suavizado', color='blue')
+plt.plot(fcast, label='Forecast', color='red')
+plt.xlabel('Año')
+plt.ylabel('Viajeros')
+plt.title('Suavizado simple')
 plt.legend()
-plt.grid()
 plt.show()
 
-# Vamos a aplicar el metodo del codo
-wcss = []
-for k in range(1, 11):  
-    kmeans = KMeans(n_clusters=k, random_state=123456)
-    kmeans.fit(penguins_std)
-    wcss.append(kmeans.inertia_)  # Inertia is the WCSS value
+print(model.params_formatted)
 
-plt.figure(figsize=(8, 6))
-plt.plot(range(1, 11), wcss, marker='o', linestyle='-', color='b')
-plt.title('Método del codo')
-plt.xlabel('Número de cluster (K)')
-plt.ylabel('WCSS')
-plt.grid(True)
+
+# Aplicamos el método de Holt
+model1 = Holt(train, initialization_method="estimated").fit()
+fcast1 = model1.forecast(12)
+print(model1.params_formatted)
+print(fcast1)
+
+plt.figure(figsize=(12, 8))
+plt.plot(train, label='Datos train', color='gray')
+plt.plot(test, label='Datos test', color='yellow')
+plt.plot(model1.fittedvalues, label='Suavizado', color='blue')
+plt.plot(fcast1, label='Forecast', color='red')
+plt.xlabel('Año')
+plt.ylabel('Viajeros')
+plt.title('Suavizado Holt')
+plt.legend()
 plt.show()
 
-# Aplicamos el método de las siluetas  ############ REVISAR
-silhouette_scores = []
-for k in range(2, 11):
-    kmeans = KMeans(n_clusters=k, random_state=0)
-    kmeans.fit(penguins_std)
-    labels = kmeans.labels_
-    silhouette_avg = silhouette_score(penguins_std, labels)
-    silhouette_scores.append(silhouette_avg)
 
-plt.figure(figsize=(8, 6))
-plt.plot(range(2, 11), silhouette_scores, marker='o', linestyle='-', color='b')
-plt.title('Método de la silueta')
-plt.xlabel('Número de Clusters (K)')
-plt.ylabel('Silhouette Score')
-plt.grid(True)
+# Aplicamos el método de la tendencia amortiguada
+model2 = Holt(train,damped_trend=True, initialization_method="estimated").fit()
+fcast2 = model2.forecast(5)
+print(model2.params_formatted)
+
+plt.figure(figsize=(12, 8))
+plt.plot(viajeros, label='Datos', color='gray')
+plt.plot(fcast, label='ses', color='red')
+plt.plot(fcast1, label='Holt', color='blue')
+plt.plot(fcast2,label="Damped",color='green')
+plt.xlabel('Año')
+plt.ylabel('Viajeros')
+plt.title('Comparativa suavizados')
+plt.legend()
 plt.show()
 
-# Vemos los valores de la silueta
-kmeans = KMeans(n_clusters=num_c, random_state=0)
-kmeans.fit(penguins_std)
-labels = kmeans.labels_
 
-silhouette_values = silhouette_samples(penguins_std, labels)
-silhouette_values
+# Aplicamos el método de Holt-Winters
+model3 = ExponentialSmoothing(train, seasonal_periods=12, trend="add", 
+                              seasonal="add", initialization_method="estimated").fit()
+fcast3 = model3.forecast(12)
+print(fcast3)
 
-# Dibujamos el grafico
-plt.figure(figsize=(8, 6))
-y_lower = 10
-for i in range(num_c):
-    ith_cluster_silhouette_values = silhouette_values[labels == i]
-    ith_cluster_silhouette_values.sort()
+plt.figure(figsize=(12, 8))
+plt.plot(train, label='Train', color='gray')
+plt.plot(test, label='Test', color='yellow')
+plt.plot(model3.fittedvalues, label='suavizado', color='blue')
+plt.plot(fcast3,color='red', label="Prediciones")
+plt.xlabel('Año')
+plt.ylabel('Viajeros')
+plt.title('Holt-Winter Aditivo')
+plt.legend()
 
-    size_cluster_i = ith_cluster_silhouette_values.shape[0]
-    y_upper = y_lower + size_cluster_i
+#Mostramos los parámetros
+headers = ['Name', 'Param', 'Value', 'Optimized']
+table_str = tabulate(model3.params_formatted, headers, tablefmt='fancy_grid')
+print(table_str)
 
-    color = plt.cm.get_cmap("Spectral")(float(i) / 4)
-    plt.fill_betweenx(np.arange(y_lower, y_upper),
-                      0, ith_cluster_silhouette_values,
-                      facecolor=color, edgecolor=color, alpha=0.7)
-
-    plt.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
-    y_lower = y_upper + 10
-
-plt.title("Gráfico de Sileuta para los Clusters")
-plt.xlabel("Valores del coeficiente de silueta")
-plt.ylabel("Etiqueta de cluster")
-plt.grid(True)
+# Mostramos la evolución de los componentes
+fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(12, 8))
+axes[0].plot(model3.level)
+axes[0].set_title('Level')
+axes[1].plot(model3.trend)
+axes[1].set_title('Trend')
+axes[2].plot(model3.season)
+axes[2].set_title('Season')
+plt.tight_layout()
 plt.show()
 
-# Caracterizamos los clusters
-penguins_std['cluster'] = labels
-penguins_std_sort = penguins_std.sort_values(by="cluster")
-penguins_std = penguins_std.set_index(labels)
-penguins_std_sort['cluster']
-
-# Calculamos los centroides
-cluster_centroids = penguins_std_sort.groupby('cluster').mean()
-cluster_centroids.round(2)
 
 
-# Repetimos pero con los datos originales, sin estandarizar
-penguins['cluster'] = labels
-penguins_sort = penguins.sort_values(by="cluster")
-penguins = penguins.set_index(labels)
-penguins_sort['cluster']
 
-cluster_centroids = penguins_sort.groupby('cluster').mean()
-print(cluster_centroids.drop('Cluster4',axis=1).round(2).to_string())
 
-penguins_original['cluster'] = labels
-print(penguins_original.groupby(['cluster', 'species']).size())
-print(penguins_original.groupby(['cluster', 'island']).size())
-print(penguins_original.groupby(['cluster', 'species', 'sex']).size())
-print(penguins_original.groupby(['cluster', 'island', 'sex']).size())
+
+
+
+
